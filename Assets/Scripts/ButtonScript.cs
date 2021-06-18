fa428677ddd2fcea949bf6494e572a17d06d7c73
ssjkkp <ssjkkp@gmail.com>
2021-06-19 00:07:05 +0300

    Basic functions for increasing and decreasing the score while the button is pressed

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool buttonPressed;
+    public static int score;
+
+    public void Update()
+    {
+        if (buttonPressed && StatusScript.press)
+        {
+            score++;
+        }
+        if(buttonPressed && !StatusScript.press)
+        {
+            score--;
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonPressed = false;
+    }
+}
---
+++ Assets/Scripts/StatusScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatusScript : MonoBehaviour
+{
+    public static bool press;
+    public TextMeshProUGUI statusText;
+    public TextMeshProUGUI scoreText;
+
+    private void Update()
+    {
+
+        scoreText.SetText(ButtonScript.score.ToString());
+    }
+    private void Start()
+    {
+        press = true;
+
+        StartCoroutine("PressReleaseCoroutine");
+    }
+
+    IEnumerator PressReleaseCoroutine()
+    {
+    for (var i = 0; i < 20; i++)
+    {
+        press = !press;
+        if (press)
+        {
+            statusText.SetText("on");
+        }
+        else
+        {
+            statusText.SetText("off");
+        }
+        yield return new WaitForSeconds(3);
+        }
+    }
+}
